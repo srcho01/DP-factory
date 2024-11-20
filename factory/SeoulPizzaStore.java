f7942ddebb01c4acff08b6df869c3cbca1a64c35
srcho01 <seorim5306@gmail.com>
2024-11-20 18:25:51 +0900

    factory: add Seoul Store

---
+++ factory/SeoulPizzaStore.java
@@ -0,0 +1,14 @@
+public class SeoulPizzaStore extends PizzaStore {
+
+	Pizza createPizza(String item) {
+		if (item.equals("cheese")) {
+			return new SeoulStyleCheesePizza();
+		} else if (item.equals("veggie")) {
+			return new SeoulStyleVeggiePizza();
+		} else if (item.equals("clam")) {
+			return new SeoulStyleClamPizza();
+		} else if (item.equals("kimchi")) {
+			return new SeoulStyleKimchiPizza();
+		} else return null;
+	}
+}
